f5410a4d5fd28d2254433e50469bfd0519c833b9
pavit <pavitharan2020@gmail.com>
2023-01-27 09:53:02 +0530

    done add, update and delete

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,17 +1,32 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { ENDPOINTS } from '../endpoints/rest.endpoints';
+import { Observable } from 'rxjs';
+import { User } from '../user';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
+  private apiServerUrl = '';
+
   constructor(private http:HttpClient) { }
   
   signIn(user: any){
 return this.http.post(ENDPOINTS.LOGIN,user).toPromise();
-         // return this.http.post("http://localhost:8080/api/user/auth",user).toPromise();
-         //return this.http.post(ENDPOINTS.LOGIN,user);
+  }
+
+  public getUsers(): Observable<User[]>{
+    return this.http.get<User[]>(`${this.apiServerUrl}/user/all`);
+  }
+
+  public addUser(user: User): Observable<User>{
+    return this.http.post<User>(`${this.apiServerUrl}/user/update`,user);
+  }
+  
+  public deleteUser(userId: number): Observable<void>{
+    return this.http.delete<void>(`${this.apiServerUrl}/user/delete/${userId}`);
   }
 }
---
+++ src/app/user.ts
@@ -0,0 +1,9 @@
+export interface User{
+    id: number;
+    name: string;
+    email: string;
+    role: string;
+    phone_no: string;
+    imageUrl: string;
+    user_code: string;
+}
