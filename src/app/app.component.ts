d58c3c1fdd51b4ff078ffdf938d59eff0c7b13dd
pavit <pavitharan2020@gmail.com>
2023-01-31 21:13:15 +0530

    app.component adduser function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'my-first-project';
   loggedInUser: string | null = null;
   loggpass: string | null=null;
@@ -22,5 +26,26 @@ export class AppComponent {
      this.role=localStorage.getItem('role');
     // this.role=localStorage.getItem('role');
     // console.log("dg"+this.loggedInUser+"x");
+
+    this.getUsers;
 }
+  
+  
+  public users: User[];
+
+  constructor(private userService: UserService){};
+
+  public getUsers(): void{
+    this.userService.getUsers().subscribe(
+        (response: User[]) => {
+          this.users = response;
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+        }
+    );
+  }
+
+
 }
+
